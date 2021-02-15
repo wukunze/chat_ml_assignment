c79123bd7fe2e6bd3c5fae5318013c97216fed9e
Hoang Pham <lehoangpham1092@gmail.com>
2021-02-14 22:33:59 -0600

    # Added migration for student table.

---
+++ cse_site/ubs_project/models/student.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Student(models.Model):
+    """Represents a Student/user."""
+    first_name = models.CharField(max_length = 100)
+    last_name = models.CharField(max_length = 100)
+    email = models.CharField(max_length = 254)
+    password = models.TextField()
+    created_at = models.DateTimeField()
