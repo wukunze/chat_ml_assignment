c6c97e25e6b7380366d1601e03474a214d3ef4f7
Hoang Pham <lehoangpham1092@gmail.com>
2021-03-06 13:47:03 -0600

    # Finished Story 4 - Login.

--- cse_site/cse_site/settings.py
+++ cse_site/cse_site/settings.py
@@ -130,8 +130,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS=(
-    os.path.join(BASE_DIR, "static"),
-)
 
 LOGIN_REDIRECT_URL = "home"
--- cse_site/cse_site/urls.py
+++ cse_site/cse_site/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,3 +26,6 @@ urlpatterns = [
     # Let Django search for authentication templates.
     path("accounts/", include("django.contrib.auth.urls"))
 ]
+
+# Use static() to add url mapping to serve static files during development (only)
+urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
